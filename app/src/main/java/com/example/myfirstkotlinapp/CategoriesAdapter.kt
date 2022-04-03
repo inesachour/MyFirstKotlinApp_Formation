d8732db781467397239ff86b7ed6166ed03f5b42
unknown <inesachour@insat.u-carthage.tn>
2022-04-03 10:29:52 +0200

    changement

--- app/src/main/java/com/example/myfirstkotlinapp/CategoriesAdapter.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/CategoriesAdapter.kt
@@ -1,12 +1,10 @@
 package com.example.myfirstkotlinapp
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoriesAdapter(private val categories: List<CategoryModel>) : RecyclerView.Adapter<CategoriesAdapter.ViewHolder>() {
